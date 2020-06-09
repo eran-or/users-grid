a5bd778be5f2fb711e60b7b95a872967c278732d
System Administrator <root@Erans-MacBook-Pro.local>
2020-06-09 16:05:56 +0300

    Updates

---
+++ precache-manifest.605460f7ad2aa948e31812de06b34b29.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de47d6243e54d75638c9882dd0a55f11",
+    "url": "/users-grid/index.html"
+  },
+  {
+    "revision": "e799f99426af87aeb1af",
+    "url": "/users-grid/static/css/2.190caba9.chunk.css"
+  },
+  {
+    "revision": "e6000266a7de446e359c",
+    "url": "/users-grid/static/css/main.e11a3558.chunk.css"
+  },
+  {
+    "revision": "e799f99426af87aeb1af",
+    "url": "/users-grid/static/js/2.adfbba23.chunk.js"
+  },
+  {
+    "revision": "0a76a262a4445af70c855f5f082cf224",
+    "url": "/users-grid/static/js/2.adfbba23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6000266a7de446e359c",
+    "url": "/users-grid/static/js/main.84364ebb.chunk.js"
+  },
+  {
+    "revision": "643cf4ece2c37a584069",
+    "url": "/users-grid/static/js/runtime-main.11a6222d.js"
+  },
+  {
+    "revision": "7d848bdff135b3050793ae163e4011ad",
+    "url": "/users-grid/static/media/logo_icon_inverse.7d848bdf.svg"
+  },
+  {
+    "revision": "badead22649e0a457cafc0f2af02c580",
+    "url": "/users-grid/static/media/medigate-logo.badead22.svg"
+  },
+  {
+    "revision": "b006b1b042712758eb8a1d0c31a551ae",
+    "url": "/users-grid/static/media/page2.b006b1b0.jpg"
+  }
+]);
